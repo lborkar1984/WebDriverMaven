ec62a33e4bcb8d0240d0824186c61bfc0076a0b9
lokesh <lokesh@lokesh-pc>
2019-03-10 22:57:52 -0700

    initial commit

---
+++ src/test/java/webdriverMaven/loginTest.java
@@ -0,0 +1,24 @@
+package webdriverMaven;
+
+	
+	import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.ChromeDriver;
+    //import org.testng.annotations.Test;
+
+
+
+	public class loginTest {
+
+	 
+	@Test
+	public void testLogin(){
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\software\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com");
+		driver.quit();
+	}
+
+}
